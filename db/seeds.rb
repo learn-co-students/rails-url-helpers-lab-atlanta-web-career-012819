0ecd7dc22e9ef4415711820427ed11191e9eb0f2
wulman16 <will.ulman@gmail.com>
2019-02-24 16:46:41 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+mike = Student.create(first_name: "Mike", last_name: "Johnson")
+sandy = Student.create(first_name: "Sandy", last_name: "Hernandez")
+joe = Student.create(first_name: "Joe", last_name: "Graybill")
